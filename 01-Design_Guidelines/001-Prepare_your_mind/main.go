8c69269775294bc4b0a492e1e88a920b8746ef5e
Dan Shoemaker <dan@shoeman-tech.com>
2026-02-10 23:11:23 -0800

    completed module 001

---
+++ 01-Design_Guidelines/001-Prepare_your_mind/main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var zz string = "#############################################################"
+	fmt.Println(zz)
+
+	var aa string = "Prepare your mind"
+	fmt.Println(aa)
+
+	var ab string = "- We became impressed with programs that contain large amounts of code"
+	fmt.Println(ab)
+
+	var ac string = "- We strived to create large abstractions in our code base"
+	fmt.Println(ac)
+
+	var ad string = "- We forgot that the hardware is the platform"
+	fmt.Println(ad)
+
+	var ae string = "- We lost the understanding that every decision comes with a cost"
+	fmt.Println(ae)
+	fmt.Println(zz)
+
+	var ba string = "Those days are gone"
+	fmt.Println(ba)
+
+	var bb string = "- We can throw more hardware at the problem"
+	fmt.Println(bb)
+
+	var bc string = "- We can throw more developers at the problem"
+	fmt.Println(bc)
+	fmt.Println(zz)
+
+	var ca string = "Open Your Mind"
+	fmt.Println(ca)
+
+	var cb string = "- Technology changes quickly but people's minds change slowly"
+	fmt.Println(cb)
+
+	var cc string = "- Easy to adopt new technology but hard to adopt new ways of thinking"
+	fmt.Println(cc)
+	fmt.Println(zz)
+
+	Quotes()
+
+	var da string = "Interesting Questions - What do they mean to you?"
+	fmt.Println(da)
+
+	var db string = "- Is it a good program?"
+	fmt.Println(db)
+
+	var dc string = "- Is it an efficient program?"
+	fmt.Println(dc)
+
+	var dd string = "- Is it correct?"
+	fmt.Println(dd)
+
+	var de string = "- Was it done on time?"
+	fmt.Println(de)
+
+	var df string = "- What did it cost?"
+	fmt.Println(df)
+	fmt.Println(zz)
+
+	var ea string = "Aspire to"
+	fmt.Println(ea)
+
+	var eb string = "- Be a champion for quality, efficiency, and simplicity"
+	fmt.Println(eb)
+
+	var ec string = "- Have a point of view"
+	fmt.Println(ec)
+
+	var ed string = "- Value introspection and self-review"
+	fmt.Println(ed)
+	fmt.Println(zz)
+}
+
+func Quotes() {
+	var aa string = "If most people lack understanding and knowledge, then what will they select will also be lacking - Alan Kay"
+	var ab string = "The software business is one of the few places we teach people to write before we teach them to read - Tom Love(inventor of Objective C)"
+	var ac string = "Programming is, among other things, a kind of writing"
+	var ad string = "One way to learn writing is to write, but in all other forms of writing, one also reads"
+	var ae string = "We read examples both good and bad to facilitate learning"
+	var af string = "But how many programmers learn to write programs by reading programs? - Gerald Weinberg"
+	var zz string = "#######################################################################################################"
+	fmt.Println(aa)
+	fmt.Println(ab)
+	fmt.Println(ac)
+	fmt.Println(ad)
+	fmt.Println(ae)
+	fmt.Println(af)
+	fmt.Println(zz)
+
+	var ba string = "Legacy Software"
+	var bb string = "Do you care about the legacy you are leaving behind?"
+	var bc string = "There are two kinds of software projects: those that fail, and those that turn into legacy horrors - Peter Weinberger (inventor of AWK)"
+	var bd string = "Legacy software is an unappreciated but serious problem"
+	var be string = "Legacy code may be the downfall of our civilization - Chuck Moore (inventor of Forth)"
+	var bf string = "Few programmers of any experience would contradict the assertion that most programs are modified in their lifetime"
+	var bg string = "Why then do we rarely find a program that contains any evidence of having been written with an eye to subsequent modification - Gerald M. Weinberg"
+	var bh string = "We think awful code is written by awful devs"
+	var bi string = "But in reality, it's written by reasonable devs in awful circumstances - Sarah Mei"
+	fmt.Println(ba)
+	fmt.Println(bb)
+	fmt.Println(bc)
+	fmt.Println(bd)
+	fmt.Println(be)
+	fmt.Println(bf)
+	fmt.Println(bg)
+	fmt.Println(bh)
+	fmt.Println(bi)
+	fmt.Println(zz)
+
+	var ca string = "Let's imagine a project that's going to end up with a million lines of code or more"
+	var cb string = "The probability of those projects being successful in the United States these days is very low - well under 50%"
+	var cc string = "That's debatable - Tom Love (inventor of Objective C)"
+	var cd string = "100k lines of code fit inside a box of paper - Tom Love (inventor of Objective C)"
+	var ce string = "The hardest bugs are those where your mental model of the situation is just wrong, so you can't see the problem at all - Brian Kernighan"
+	var cf string = "Everyone knows that debugging is twice as hard as writing a program in the first place"
+	var cg string = "So if you're as clever as you can be when you write it, how will you ever debug it? - Brain Kernighan"
+	var ch string = "Debuggers don't remove bugs. They only show them in slow motion - Unknown"
+	fmt.Println(ca)
+	fmt.Println(cb)
+	fmt.Println(cc)
+	fmt.Println(cd)
+	fmt.Println(ce)
+	fmt.Println(cf)
+	fmt.Println(cg)
+	fmt.Println(ch)
+	fmt.Println(zz)
+
+}
