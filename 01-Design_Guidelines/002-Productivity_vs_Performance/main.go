688f64dae915ca12c241fc32089f8dece79df6f8
Dan Shoemaker <dan@shoeman-tech.com>
2026-02-11 21:38:44 -0800

    completed module 002

---
+++ 01-Design_Guidelines/002-Productivity_vs_Performance/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+func main() {
+	var zz string = "################################################################################"
+	fmt.Println(zz)
+
+	var aa string = "The hope is that the progress in hardware will cure all software ills."
+	var ab string = "However, a critcal observer may observe that software manages to outgrow hardware in size and sluggishness."
+	var ac string = "Other observers had noted this for some time before, indeed the trend was becoming obvious as early as 1987."
+	var ad string = "- Niklaus Wirth"
+	fmt.Println(aa)
+	fmt.Println(ab)
+	fmt.Println(ac)
+	fmt.Println(ad)
+	fmt.Println(zz)
+
+	var ba string = "The most amazing achievement of the computer software industry is its continuing cancellation of"
+	var bb string = "the steady and staggering gains made by the computer hardware industry"
+	var bc string = "- Henry Petroski (2015)"
+	fmt.Println(ba)
+	fmt.Println(bb)
+	fmt.Println(bc)
+	fmt.Println(zz)
+
+	var ca string = "The hardware folks will not put more cores into their hardware if the software isn't going to use them,"
+	var cb string = "so, it is the balancing act of each other staring at each other,"
+	var cc string = "and we are hoping that Go is going to break through on the software side"
+	var cd string = "- Rick Hudson (2015)"
+	fmt.Println(ca)
+	fmt.Println(cb)
+	fmt.Println(cc)
+	fmt.Println(cd)
+	fmt.Println(zz)
+
+	var da string = "C is the best balance I've ever seen between power and expressiveness"
+	var db string = "You can do almost anything you want to do by programming fairly straightforwardly"
+	var dc string = "and you will have a very good mental model of what's going to happen on the machine;"
+	var dd string = "you can predict reasonably well how quickly it's going to run, you understand what is going on..."
+	var de string = "- Brian Kernighan (2000)"
+	fmt.Println(da)
+	fmt.Println(db)
+	fmt.Println(dc)
+	fmt.Println(dd)
+	fmt.Println(de)
+	fmt.Println(zz)
+}
