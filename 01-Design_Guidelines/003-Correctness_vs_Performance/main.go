ac1dbe425384fbc59f553b10551c7d0d1882591c
Dan Shoemaker <dan@shoeman-tech.com>
2026-02-12 21:43:46 -0800

    completed module 003

---
+++ 01-Design_Guidelines/003-Correctness_vs_Performance/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+func main() {
+	var zz string = "#############################################################################"
+	fmt.Println(zz)
+
+	var aa string = "Make it correct, make it clear, make it concise, make it fast."
+	var ab string = "In that order."
+	var ac string = "- Wes Dyer"
+	fmt.Println(aa)
+	fmt.Println(ab)
+	fmt.Println(ac)
+	fmt.Println(zz)
+
+	var ba string = "Good engineering is less about finding the 'perfect' solution"
+	var bb string = "and more about understanding the tradeoffs and being able to explain them"
+	var bc string = "- JBD"
+	fmt.Println(ba)
+	fmt.Println(bb)
+	fmt.Println(bc)
+	fmt.Println(zz)
+
+	var ca string = "The correctness of the implementation is the most important concern,"
+	var cb string = "but there is no royal road to correctness."
+	var cc string = "It involves diverse tasks such as thinking of invariants, testing and code reviews."
+	var cd string = "Optimization should be done, but not prematurely."
+	var ce string = "Al Aho (inventor of AWK)"
+	fmt.Println(ca)
+	fmt.Println(cb)
+	fmt.Println(cc)
+	fmt.Println(cd)
+	fmt.Println(ce)
+	fmt.Println(zz)
+
+	var da string = "Problems can usually be solved with simple. mundane solutions."
+	var db string = "That means there's no glamorous work."
+	var dc string = "You don't get to show off your amazing skills."
+	var dd string = "You just build something that gets the job done and then move on."
+	var de string = "This approach may not earn you oohs and aahs, but it lets you get on with it."
+	var df string = "- Jason Fried"
+	fmt.Println(da)
+	fmt.Println(db)
+	fmt.Println(dc)
+	fmt.Println(dd)
+	fmt.Println(de)
+	fmt.Println(df)
+	fmt.Println(zz)
+}
