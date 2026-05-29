9c4e2838aeeb5531e534aae66f3c3e5299dd57c2
Daniel Shoemaker <dshoemak@dshoemak-thinkcentrem70tgen6.rmtusor.csb>
2026-05-28 18:45:21 -0700

    completed module 005

---
+++ 01-Design_Guidelines/005-If_Performance_Matters/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var zz string = "####################################################################"
+	fmt.Println(zz)
+
+	var aa string = "Four reasons why my software is not running as fast as it could be"
+	var ab string = "1 - External latencies"
+	var ac string = "2 - Internal latencies"
+	var ad string = "3 - Access data on the machine"
+	var ae string = "4 - Algorithm efficiency"
+	fmt.Println(aa)
+	fmt.Println(ab)
+	fmt.Println(ac)
+	fmt.Println(ad)
+	fmt.Println(ae)
+	fmt.Println(zz)
+}
