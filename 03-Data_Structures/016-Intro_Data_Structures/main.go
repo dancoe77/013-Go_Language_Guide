25c9703cc3e8207021fcab8854650eee8d1f1fd2
Dan S <dan@shoeman-tech.com>
2026-06-09 17:58:31 -0700

    completed module 016

---
+++ 03-Data_Structures/016-Intro_Data_Structures/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	zz := "###############################################################"
+	fmt.Println(zz)
+
+	aa := "Learn about arrays"
+	ab := "Learn about maps"
+	ac := "Learn about slices"
+	fmt.Println(aa)
+	fmt.Println(ab)
+	fmt.Println(ac)
+	fmt.Println(zz)
+
+	ad := "Slices are Go's most important data structure"
+	ae := "Going to learn also about the hardware"
+	af := "Learn about the mechanical sympathies that the hardware can give us"
+	fmt.Println(ad)
+	fmt.Println(ae)
+	fmt.Println(af)
+	fmt.Println(zz)
+}
