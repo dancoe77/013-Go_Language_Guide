72924b87847b73463a604f6c4e6e3d9e5e2952a0
Dan S <dan@shoeman-tech.com>
2026-06-16 20:42:13 -0700

    completed module 025

---
+++ 03-Data_Structures/025-Slices_6/main.go
@@ -0,0 +1,28 @@
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Sample program to show how the for range has both value and pointer semantics.
+package main
+
+import "fmt"
+
+func main() {
+
+	// Using the value semantic form of the for range.
+	friends := []string{"Annie", "Betty", "Charlie", "Doug", "Edward"}
+	for _, v := range friends {
+		friends = friends[:2]
+		fmt.Printf("v[%s]\n", v)
+	}
+	fmt.Println(friends)
+
+	fmt.Println()
+
+	// Using the pointer semantic form of the for range.
+	friends = []string{"Annie", "Betty", "Charlie", "Doug", "Edward"}
+	for i := range friends {
+		// friends = friends[:2] // This will not work
+		friends = friends[:5] // but this one will
+		fmt.Printf("p[%s]\n", friends[i])
+	}
+}
