21b76375114934a3dabcc6e18e6bb0bdfeaea0c8
Dan S <dan@shoeman-tech.com>
2026-06-17 22:00:47 -0700

    completed module 027

---
+++ 04-Decoupling/027-Intro_Decoupling/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	zz := "###############################################################"
+	fmt.Println(zz)
+
+	aa := "This section teaches us how to decouple our code from changes"
+	ab := "those changes are centered around data changes"
+	ac := "Will also put learning about decoupling semantics"
+	fmt.Println(aa)
+	fmt.Println(ab)
+	fmt.Println(ac)
+	fmt.Println(zz)
+}
