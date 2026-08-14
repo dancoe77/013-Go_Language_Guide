0ff4175381a90d5de5450be3f48a51c01919762d
Dan S <dan@shoeman-tech.com>
2026-08-13 21:31:32 -0700

    completed module 039

---
+++ 05-Composition/039-Conversion_and_Assertion_2/main.go
@@ -0,0 +1,151 @@
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Sample program demonstrating when implicit interface conversions
+// are provided by the compiler.
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+// Mover provides support for moving things.
+type Mover interface {
+	Move()
+}
+
+// Locker provides support for locking and unlocking things.
+type Locker interface {
+	Lock()
+	Unlock()
+}
+
+// Movelocker provides support for moving and unlocking things.
+type MoveLocker interface {
+	Mover
+	Locker
+}
+
+// bike represents a concrete method type for the example.
+type bike struct{}
+
+// Move can change the position of a bike.
+func (bike) Move() {
+	fmt.Println("moving the bike")
+}
+
+// Lock prevents a bike from moving.
+func (bike) Lock() {
+	fmt.Println("Locking the bike")
+}
+
+// Unlock allows a bike to be moved.
+func (bike) Unlock() {
+	fmt.Println("Unlocking the bike")
+}
+
+// car represents something you drive.
+type car struct {}
+
+// String implements the fmt.Stringer interface.
+func (car) String() string {
+	return "Vroom!"
+}
+
+// cloud represents somewhere you store information.
+type cloud struct{}
+
+// String implements the fmt.Stringer interface.
+func (cloud) String() string {
+	return "Big Data!"
+}
+
+// user defines a user in the system.
+type user struct {
+	name string
+	email string
+}
+
+// String implements the fmt.Stringer interface
+func (u *user) String() string {
+	return fmt.Sprintf("My name is %q and my email is %q", u.name, u.email)
+}
+
+func main() {
+	// Declare variables of the MoveLocker and Mover interfaces set to their
+	// zero value.
+	var ml MoveLocker
+	var m Mover
+
+	// Create a value of type bike and assign the value to the Movelocker
+	// interface value.
+	ml = bike{}
+
+	// An interface value of type MoveLocker can be implicitly converted into
+	// a value of type Mover. They both declare a method named move.
+	m = ml
+
+	// prog.go:68 cannot use m (type Mover) as type MoveLocker in assignment:
+	//      Mover does not implement MoveLocker (missing Lock method)
+	// ml = m
+
+	// Interface type Mover does not declare methods named lock and unlock.
+	// Therefore, the compiler can't perform an implicit conversion to assign
+	// a value of interface type Mover to an interface value of type MoveLocker.
+	// It is irrelevant that the concrete type value of type bike that is stored
+	// inside of the Mover interface value implements the MoveLocker interface.
+
+	// We can perform a type assertion at runtime to support the assignment.
+
+	// Perform a type assertion against the Mover interface value to access
+	// a COPY of the concrete type value of type bike that was stored inside
+	// of it. Then assign the COPY of the concrete type to the MoveLocker
+	// interface.
+	b := m.(bike) // b, ok := m.(bike)
+	ml = b
+
+	// It's important to note that the type assertion syntax provides a way
+	// to state what type of value is stored inside the interface. This is
+	// more powerful from a language and readability standpoint, than using
+	// a casting syntax, like in other languages.
+
+	// #########################################################################
+
+	// Seed the number random generator.
+	rand.NewSource(time.Now().UnixNano())
+
+	// Create a slice of the Stringer interface values.
+	mvs := []fmt.Stringer{
+		car{},
+		cloud{},
+	}
+
+	// Let's run this experiment ten times.
+	for i := 0; i < 10; i++ {
+
+		// Choose a random number from 0 to 1.
+		rn := rand.Intn(2)
+
+		// Perform a type assertion that we have a concrete type
+		// of cloud in the interface value we randomly chose.
+		if v, is := mvs[rn].(cloud); is {
+			fmt.Println("Got Lucky", v)
+			continue
+		}
+		fmt.Println("Got Unlucky")
+	}
+
+	// ######################################################################
+
+	// Create a value of type user.
+	u := user {
+		name: "Bill",
+		email: "bill@ardanlabs.com",
+	}
+
+	// Display the values.
+	fmt.Println(u)
+	fmt.Println(&u)
+}
