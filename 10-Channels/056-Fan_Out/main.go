c4afd28fceeb371a9cd5db84fc6511577e2e5c86
Dan S <dan@shoeman-tech.com>
2026-09-01 19:37:55 -0700

    completed module 056

---
+++ 10-Channels/056-Fan_Out/main.go
@@ -0,0 +1,64 @@
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// This sample program demonstrates the basic channel mechanics
+// for goroutine signaling.
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func init() {
+	rand.NewSource(time.Now().UnixNano())
+}
+
+func main() {
+
+	waitForResult()
+}
+
+// waitForResult: In this pattern, the parent goroutine waits for the child
+// goroutine to finish some work to signal the result.
+func waitForResult() {
+	ch := make(chan string)
+
+	go func() {
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		ch <- "data"
+		fmt.Println("child : sent signal")
+	}()
+
+	d := <-ch
+	fmt.Println("parent : recv'd signal :", d)
+
+	time.Sleep(time.Second)
+	fmt.Println("---------------------------------------------------------------------")
+}
+
+// fanOut: In this pattern, the parent goroutine creates 2000 child goroutines
+// and waits for them to signal their results.
+func fanOut() {
+	children := 2000
+	ch := make(chan string, children)
+
+	for c := 0; c < children; c++ {
+		go func(child int) {
+			time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
+			ch <- "data"
+			fmt.Println("child : sent signal :", children)
+		}(c)
+	}
+
+	for children > 0 {
+		d := <-ch
+		children--
+		fmt.Println(d)
+		fmt.Println("parent : recv'd signal :", children)
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println("---------------------------------------------------------------------")
+}
