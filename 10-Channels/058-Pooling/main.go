890998ef866a6efb543930998521ab088ae70696
Dan S <dan@shoeman-tech.com>
2026-09-02 19:56:13 -0700

    completed module 058

---
+++ 10-Channels/058-Pooling/main.go
@@ -0,0 +1,115 @@
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// This sample program demonstrates the basic channel mechanics
+// for goroutine signaling.
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"runtime"
+	"time"
+)
+
+func init() {
+	rand.NewSource(time.Now().UnixNano())
+}
+
+func main() {
+
+	waitForResult()
+	// fanOut()
+
+	// waitForTask()
+	// pooling()
+}
+
+// waitForResult: In this pattern, the parent goroutine waits for the child
+// goroutine to finish some work to signal the result.
+func waitForResult() {
+	ch := make(chan string)
+
+	go func() {
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		ch <- "data"
+		fmt.Println("child : sent signal")
+	}()
+
+	d := <-ch
+	fmt.Println("parent : recv'd signal :", d)
+
+	time.Sleep(time.Second)
+	fmt.Println("---------------------------------------------------------------------")
+}
+
+// fanOut: In this pattern, the parent goroutine creates 2000 child goroutines
+// and waits for them to signal their results.
+func fanOut() {
+	children := 2000
+	ch := make(chan string, children)
+
+	for c := 0; c < children; c++ {
+		go func(child int) {
+			time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
+			ch <- "data"
+			fmt.Println("child : sent signal :", children)
+		}(c)
+	}
+
+	for children > 0 {
+		d := <-ch
+		children--
+		fmt.Println(d)
+		fmt.Println("parent : recv'd signal :", children)
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println("---------------------------------------------------------------------")
+}
+
+// waitForTask: In this pattern, the parent goroutine sends a signal to a
+// child goroutine waiting to be told what to do.
+func waitForTask() {
+	ch := make(chan string)
+
+	go func() {
+		d := <-ch
+		fmt.Println("child : recv'd signal :", d)
+	}()
+
+	time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+	ch <- "data"
+	fmt.Println("parent : sent signal")
+
+	time.Sleep(time.Second)
+	fmt.Println("------------------------------------------------------------------")
+}
+
+// pooling: In this pattern, the parent goroutine signals 100 pieces of work
+// to a pool of child goroutines waiting for work to perform.
+func pooling() {
+	ch := make(chan string)
+
+	g := runtime.NumCPU()
+	for c := 0; c < g; c++ {
+		go func(child int) {
+			for d := range ch {
+				fmt.Printf("child %d : recv'd signal : %s\n", child, d)
+			}
+			fmt.Printf("child %d : recv'd shutdown signal\n", child)
+		}(c)
+	}
+
+	const work = 100
+	for w := 0; w < work; w++ {
+		ch <- "data"
+		fmt.Println("parent : sent signal :", w)
+	}
+
+	close(ch)
+	fmt.Println(" parent : sent shutdown signal")
+
+	time.Sleep(time.Second)
+	fmt.Println("--------------------------------------------------------------------")
+}
