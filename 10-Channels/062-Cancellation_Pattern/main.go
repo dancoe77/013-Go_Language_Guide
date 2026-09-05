ad759ae9ccca84dfa041b046c273967fe4efee36
Dan S <dan@shoeman-tech.com>
2026-09-04 21:59:33 -0700

    created logger application

---
+++ 10-Channels/062-Cancellation_Pattern/main.go
@@ -0,0 +1,247 @@
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// This sample program demonstrates the basic channel mechanics
+// for goroutine signaling.
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"runtime"
+	"sync"
+	"time"
+)
+
+func init() {
+	rand.NewSource(time.Now().UnixNano())
+}
+
+func main() {
+
+	waitForResult()
+	// fanOut()
+
+	// waitForTask()
+	// pooling()
+
+	// Advanced patterns
+	//		fanOutSem()
+	//      boundedWorkPooling()
+	//      drop()
+
+	// Cancellation Pattern
+	//      cancellation()
+}
+
+// waitForResult: In this pattern, the parent goroutine waits for the child
+// goroutine to finish some work to signal the result.
+func waitForResult() {
+	ch := make(chan string)
+
+	go func() {
+		time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+		ch <- "data"
+		fmt.Println("child : sent signal")
+	}()
+
+	d := <-ch
+	fmt.Println("parent : recv'd signal :", d)
+
+	time.Sleep(time.Second)
+	fmt.Println("---------------------------------------------------------------------")
+}
+
+// fanOut: In this pattern, the parent goroutine creates 2000 child goroutines
+// and waits for them to signal their results.
+func fanOut() {
+	children := 2000
+	ch := make(chan string, children)
+
+	for c := 0; c < children; c++ {
+		go func(child int) {
+			time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
+			ch <- "data"
+			fmt.Println("child : sent signal :", children)
+		}(c)
+	}
+
+	for children > 0 {
+		d := <-ch
+		children--
+		fmt.Println(d)
+		fmt.Println("parent : recv'd signal :", children)
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println("---------------------------------------------------------------------")
+}
+
+// waitForTask: In this pattern, the parent goroutine sends a signal to a
+// child goroutine waiting to be told what to do.
+func waitForTask() {
+	ch := make(chan string)
+
+	go func() {
+		d := <-ch
+		fmt.Println("child : recv'd signal :", d)
+	}()
+
+	time.Sleep(time.Duration(rand.Intn(500)) * time.Millisecond)
+	ch <- "data"
+	fmt.Println("parent : sent signal")
+
+	time.Sleep(time.Second)
+	fmt.Println("------------------------------------------------------------------")
+}
+
+// pooling: In this pattern, the parent goroutine signals 100 pieces of work
+// to a pool of child goroutines waiting for work to perform.
+func pooling() {
+	ch := make(chan string)
+
+	g := runtime.NumCPU()
+	for c := 0; c < g; c++ {
+		go func(child int) {
+			for d := range ch {
+				fmt.Printf("child %d : recv'd signal : %s\n", child, d)
+			}
+			fmt.Printf("child %d : recv'd shutdown signal\n", child)
+		}(c)
+	}
+
+	const work = 100
+	for w := 0; w < work; w++ {
+		ch <- "data"
+		fmt.Println("parent : sent signal :", w)
+	}
+
+	close(ch)
+	fmt.Println(" parent : sent shutdown signal")
+
+	time.Sleep(time.Second)
+	fmt.Println("--------------------------------------------------------------------")
+}
+
+// fanOutSem: In this pattern, a semaphore is added to the fan out pattern
+// to restrict the number of child goroutines that can be schedule to run.
+func fanOutSem() {
+	children := 2000
+	ch := make(chan string, children)
+
+	g := runtime.NumCPU()
+	sem := make(chan bool, g)
+
+	for c := 0; c < children; c++ {
+		go func(child int) {
+			sem <- true
+			{
+				t := time.Duration(rand.Intn(200)) * time.Millisecond
+				time.Sleep(t)
+				ch <- "data"
+				fmt.Println("child : sent signal :", child)
+			}
+			<-sem
+		}(c)
+	}
+
+	for children > 0 {
+		d := <-ch
+		children--
+		fmt.Println(d)
+		fmt.Println("parent : recv'd signal :", children)
+	}
+
+	time.Sleep(time.Second)
+	fmt.Println("-----------------------------------------------------")
+}
+
+// boundedWorkPooling: In this pattern, a pool of child goroutines is created
+// to service a fixed amount of work. The parent goroutine iterates over all
+// work, signalling that into the pool. Once all the work has been signaled,
+// then the channel is closed, the channel is flushed, and the child
+// goroutines terminate.
+func boundedWorkPooling() {
+	work := []string{"paper", "paper", "paper", "paper", 2000: "paper"}
+
+	g := runtime.NumCPU()
+	var wg sync.WaitGroup
+	wg.Add(g)
+
+	ch := make(chan string, g)
+
+	for c := 0; c < g; c++ {
+		go func(child int) {
+			defer wg.Done()
+			for wrk := range ch {
+				fmt.Printf("child %d : recv'd signal : %s\n", child, wrk)
+			}
+			fmt.Printf("child %d : recv'd shutdown signal\n", child)
+		}(c)
+	}
+	for _, wrk := range work {
+		ch <- wrk
+	}
+	close(ch)
+	wg.Wait()
+
+	time.Sleep(time.Second)
+	fmt.Println("---------------------------------------------------------------")
+}
+
+// drop: In this pattern, the parent goroutine signals 2000 pieces of work to
+// a single child goroutine that can't handle all the work. If the parent
+// performs a send and the child is not ready, that work is discarded and dropped.
+func drop() {
+	const cap = 120
+	ch := make(chan string, cap)
+
+	go func() {
+		for p := range ch {
+			fmt.Println("child : recv'd signal :", p)
+		}
+	}()
+
+	const work = 2000
+	for w := 0; w < work; w++ {
+		select {
+		case ch <- "data":
+			fmt.Println("parent : sent signal :", w)
+		default:
+			fmt.Println("parent : dropped data :", w)
+		}
+	}
+	close(ch)
+	fmt.Println("parent : sent shutdown signal")
+
+	time.Sleep(time.Second)
+	fmt.Println("-----------------------------------------------------------------------------")
+}
+
+// cancellation: In this pattern, the parent goroutine creates a child
+// goroutine to perform some work. The parent goroutine is only willing to
+// wait 150 milliseconds for that work to be completed. After 150 milliseconds
+// the parent goroutine walks away.
+func cancellation() {
+	duration := 150 * time.Millisecond
+	ctx, cancel := context.WithTimeout(context.Background(), duration)
+	defer cancel()
+
+	ch := make(chan string, 1)
+
+	go func() {
+		time.Sleep(time.Duration(rand.Intn(200)) * time.Millisecond)
+		ch <- "data"
+	}()
+
+	select {
+	case d := <-ch:
+		fmt.Println("work complete", d)
+
+	case <- ctx.Done():
+		fmt.Println("work cancelled")
+	}
+	time.Sleep(time.Second)
+	fmt.Println("-------------------------------------------------------------------------")
+}
