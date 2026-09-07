c1067f4ff9c7428992aa39fa3e9cbfe4353973ef
Dan S <dan@shoeman-tech.com>
2026-09-06 21:59:14 -0700

    created handlers.go

---
+++ 12-Testing/067-Testing_Internal_Endpoints/main.go
@@ -0,0 +1,9 @@
+// All material is licensed under the Apache License Version 2.0, January 2004
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Sample program that implements a simple web service.
+package main
+
+func main() {
+
+}
