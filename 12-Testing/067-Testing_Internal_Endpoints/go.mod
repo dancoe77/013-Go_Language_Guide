cf5b1359a4f9fcf5f4a1aa4600b85d1179096ae6
Dan S <dan@shoeman-tech.com>
2026-09-06 22:44:10 -0700

    completed module 067

---
+++ 12-Testing/067-Testing_Internal_Endpoints/go.mod
@@ -0,0 +1,3 @@
+module github.com/dancoe77/013-Go_Language_Guide/12-Testing/067-Testing_Internal_Endpoints/handlers/handlers
+
+go 1.26.7
--- 12-Testing/067-Testing_Internal_Endpoints/main.go
+++ 12-Testing/067-Testing_Internal_Endpoints/main.go
@@ -4,6 +4,16 @@
 // Sample program that implements a simple web service.
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
 
+	"github.com/dancoe77/013-Go_Language_Guide/12-Testing/067-Testing_Internal_Endpoints/handlers/handlers/handlers"
+)
+
+func main() {
+	handlers.Routes()
+
+	log.Println("listener : Started : Listening on: http://localhost:400")
+	http.ListenAndServe(":4000", nil)
 }
